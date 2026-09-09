b98207a1cec5cc9c157a251e55ea1fd67ea4a29c
Sophie Pike <sophie.pike@ons.gov.uk>
2026-09-09 16:33:51 +0100

    feat: updated the example_2 code to account for correct logging and run_directory location searches. Also provides stage_results for use in demo

--- examples/pipeline_2/scripts/0_clean_data.py
+++ examples/pipeline_2/scripts/0_clean_data.py
@@ -31,7 +31,7 @@ def standardise_columns(df):
     return df
 
 
-def main(context):
+def main(context=None):
     # Load stage configuration
     config = context.get_stage_config("0_clean_data")
 
