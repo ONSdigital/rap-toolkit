b99a19f69cacfba09df0d8223ecc479855097e51
Alex Sweet <148556854+BelowBayesline@users.noreply.github.com>
2026-09-23 15:07:37 +0100

    Update docs/conf.py
    Co-authored-by: Sophie Pike_ONS <133784265+pikes-ons@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ source_suffix = {
 master_doc = "index"
 
 # General information about the project.
-project = "POPPy - Pipeline Orchestration Package for Python"
+project = "rap-toolkit"
 author = "ONSDigital"
 
 # The version info for the project you're documenting, acts as replacement for
